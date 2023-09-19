4cc7f35a86587da1c5079b1940d480a3fc804bbd
Sami <Sami.Saleh.87@gmail.com>
2023-09-19 18:00:31 +0300

    fixed bugs

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
     [SerializeField] TMP_Text answerText;
+    [SerializeField] Button button;
     public void ButtonClicked()
     {
         if (answerText.text == GameManager.GetCorrectAnswer())
+        {
+            button.image.color = Color.green;
             Debug.Log("answer is correct");
-        
+        }
+
         else
+        {
+            button.image.color = Color.red;
             Debug.Log("answer is not correct");
+        }
+            
     }
 }
